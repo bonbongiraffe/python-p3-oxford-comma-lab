cdc34bbc2016e5e25cb951200094e99cbb2179f7
bonbongiraffe <francescowai@yahoo.com>
2023-07-03 15:39:36 -0400

    first attempt

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,12 @@
 def oxford_comma(items):
-    return None
+    if ( len(items) == 1 ):
+        return (items[0])
+    if ( len(items) == 2 ):
+        return (f"{items[0]} and {items[1]}")
+    items_formatted = ''
+    for i in range (len(items)):
+        if ( i == len(items) - 1):
+            items_formatted += (f"and {items[i]}")
+        else:
+            items_formatted += (f"{items[i]}, ")
+    return items_formatted
